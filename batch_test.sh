9e0b491491e2fce60a76a9ff9b5584e453f39f70
Sizz Flair <sizzflair97@gmail.com>
2025-10-01 16:45:52 +0900

    Modified batch_test.sh

--- batch_test.sh
+++ batch_test.sh
@@ -1,25 +1,32 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
+trap "kill 0" SIGINT
 
-encoding="latency" # "baseline" or "latency"
-deltas=(1)
+encoding="latency" # "baseline" or "latency", only changes the name of the output log.
+deltas=(3)
 test_types=("mnist") # "mnist" "fmnist"
 solvers=("np") # "np" "z3" "milp"
-adv="--adv" # "--adv" or ""
-hidden_neurons=(128 256 384 512) # 128 256 384 512
-num_steps=(256) # 64 128 192 256
-repeat=5 # 1 or 5
-for hidden_neuron in ${hidden_neurons[@]}
+advs=("" "--adv") # "--adv" or ""
+hidden_neurons=(512) # 128 256 384 512
+num_steps=(64) # 64 128 192 256
+repeat=1 # number of repetitions for each setting
+for solver in ${solvers[@]}
 do
-  for num_steps in ${num_steps[@]}
+  for hidden_neuron in ${hidden_neurons[@]}
   do
-    for delta in ${deltas[@]}
+    for num_steps in ${num_steps[@]}
     do
-      for test_type in ${test_types[@]}
+      for delta in ${deltas[@]}
       do
-        for solver in ${solvers[@]}
+        for test_type in ${test_types[@]}
+        do
+          for adv in "${advs[@]}"
           do
-          python batch_test.py -p ${encoding} --delta-max ${delta} --test-type ${test_type} --${solver} --num-samples 14 --n-hidden-neurons ${hidden_neuron} --num-steps ${num_steps} ${adv} --repeat ${repeat}
+            script="python batch_test.py -p ${encoding} --delta-max ${delta} --test-type ${test_type} --${solver} --num-samples 14 --n-hidden-neurons ${hidden_neuron} --num-steps ${num_steps} ${adv} --repeat ${repeat}"
+            echo $script
+            $script &
           done
+        done
+        wait
       done
     done
   done
